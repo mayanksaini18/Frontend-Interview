3faf4c35477c6d434de6b2bc6e17a10a7b79d562
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-17 19:19:10 +0530

    QueryDevtools added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const client = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </StrictMode>,
-)
+     <ReactQueryDevtools />
+  </QueryClientProvider>
+);
+
