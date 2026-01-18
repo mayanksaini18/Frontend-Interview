9ec0d6fd7f14eb63c286500baf6fa6c73443442f
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-18 11:16:49 +0530

    clean the file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import './App.css'
+
 import { Route , Routes , Link } from 'react-router-dom';
-import BlogList from "./pages/BlogList";
+import BlogList from "./pages/Bloglist";
 import BlogDetail from "./pages/BlogDetail";
 import CreateBlog from "./pages/CreateBlog";
 function App() {
   
-
   return (
-       <>
+    <>
       <Link to="/create">Create Blog</Link>
       <Routes>
         <Route path="/" element={<BlogList />} />
