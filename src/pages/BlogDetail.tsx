15cc51d1482be27d5ca3cfc1c59bdfd48bbf1f16
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-18 11:17:28 +0530

    import blog from types

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -1,21 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getBlogById } from "../api/blog.api";
+import { Blog } from "../types";
 
 export default function BlogDetail() {
   const { id } = useParams();
 
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery<Blog>({
     queryKey: ['blog', id],
-    queryFn: () => getBlogById(Number(id))
+    queryFn: () => getBlogById(id!),
+    enabled: !!id,
   });
 
   if (isLoading) return <p>Loading...</p>
   if (isError) return <p>Error...</p>
+  if (!data) return <p>No blog found</p>
 
   return (
     <div>
       <h2>{data.title}</h2>
+      <img src={data.coverImage} alt={data.title} style={{ maxWidth: '100%' }} />
       <p>{data.content}</p>
     </div>
   );
