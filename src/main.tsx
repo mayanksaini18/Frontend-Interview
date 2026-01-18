4f99f4c1905144a27836e3660f4fe20428a13263
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-18 11:17:06 +0530

    add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import App from './App.tsx'
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
 
 const client = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={client}>
-    <App />
-     <ReactQueryDevtools />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+    <ReactQueryDevtools />
   </QueryClientProvider>
 );
-
