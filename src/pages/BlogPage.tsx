4f5691a494656a12c7c6204117b5a37e90e21c02
Mayank Saini <mayanksaini0416@gmail.com>
2026-01-19 11:29:34 +0530

    Update BlogPage.tsx

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,6 +1,6 @@
 
 import { useParams } from "react-router-dom";
-import BlogList from "./Bloglist";
+import BlogList from "./BlogList";
 import BlogDetail from "./BlogDetail";
 
 export default function BlogPage() {
